7adfa29cb85553c1dfa84d8ff9fdc47950b4d840
shahdabdelmonem910 <shahd.abdelmonem910@gmail.com>
2026-05-10 13:07:57 +0300

    add lab5

---
+++ lab5/create_image.py
@@ -0,0 +1,7 @@
+﻿from PIL import Image, ImageDraw
+img = Image.new("RGB", (1200, 800), (240, 240, 240))
+draw = ImageDraw.Draw(img)
+draw.text((50, 50), "Lecture 5 Local Serverless Lab", fill=(0, 0, 0))
+draw.text((50, 100), "Event-driven image processing pipeline", fill=(0, 0, 0))
+img.save("/data/input/test_image.png")
+print("Created /data/input/test_image.png")
